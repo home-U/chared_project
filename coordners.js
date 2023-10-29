6bba03b9af5f311188058f698be1d103a21f4fbb
elhmam-saad <149264551+elhmam-saad@users.noreply.github.com>
2023-10-29 13:50:28 +0100

    adding coordners.js

---
+++ coordners.js
@@ -0,0 +1,12 @@
+
+let coordners = {
+  x: 100,
+  y: 50,
+  z: 701
+}
+
+function showCoordners(coordners) {
+  console.log(`x: ${this.x}`);
+  console.log(`y: ${this.y}`);
+  console.log(`z: ${this.z}`);
+}
